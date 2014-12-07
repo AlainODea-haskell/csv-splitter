4830bf3b5c2578b4398dffd8c08c3a3fb75ff989
Alain O'Dea <AlainODea@users.noreply.github.com>
2014-12-06 21:28:32 -0330

    Partial code clean up
     * Split into functions
     * Use step-down rule to order declarations
    
    Still needs further work since there is an inconsistent mix of details
    and high-level policy within single declarations.

--- Main.hs
+++ Main.hs
@@ -6,31 +6,18 @@ import Control.Monad (forM_)
 
 main :: IO ()
 main = do
-    args <- getArgs
-    let csvFile = head args
-    csvData <- TL.readFile csvFile
-    let readRational x = read x :: Rational
-    let segmentFractions = map (readRational . map
-                                (\x -> case x of
-                                            '/' -> '%'
-                                            _ -> x)
-                               ) $ tail args
-    let csvLines = TL.lines csvData
-    let header = head csvLines
-    let rows = tail csvLines
-    let count = length rows
-    let segmentLengths = map ((\x -> numerator x `div` denominator x) .
-                            (\x -> x * toRational count)
-                           ) segmentFractions
-    let segmentIndices = scanl1 (+) $ 0:segmentLengths
-    let segmentIndexLengthPairs = zip segmentIndices segmentLengths
-    putStrLn $ show segmentIndexLengthPairs
-    let segmentedRows = map (\x ->
-                                  take (fromIntegral (snd x)) $
-                                  drop (fromIntegral (fst x)) rows)
-                            segmentIndexLengthPairs
-    let segmentedRowsAndIndices = zip segmentedRows [0..]
-    forM_ segmentedRowsAndIndices (\(rowsSegment,index) ->
+  args <- getArgs
+  mainWithArgs args
+
+mainWithArgs :: [String] -> IO ()
+mainWithArgs args = do
+  csvData <- TL.readFile csvFile
+  split args csvData
+  where csvFile = head args
+
+split :: [String] -> TL.Text -> IO ()
+split args csvData =
+    forM_ (segmentedRowsAndIndices args csvLines) (\(rowsSegment,index) ->
                                  if not $ null rowsSegment then
                                    do
                                     let csvSegmentFile = csvFile ++ show index ++ ".csv"
@@ -41,3 +28,26 @@ main = do
                                  else
                                    return ()
                                 )
+      where csvFile = head args
+            header = head csvLines
+            csvLines = TL.lines csvData
+
+segmentedRowsAndIndices :: [String] -> [TL.Text] -> [([TL.Text],Int)]
+segmentedRowsAndIndices args csvLines = zip segmentedRows [0..]
+  where segmentedRows = map (\x ->
+                                  take (fromIntegral (snd x)) $
+                                  drop (fromIntegral (fst x)) rows)
+                            segmentIndexLengthPairs
+        rows = tail csvLines
+        segmentIndexLengthPairs = zip segmentIndices segmentLengths
+        segmentIndices = scanl1 (+) $ 0:segmentLengths
+        segmentLengths = map ((\x -> numerator x `div` denominator x) .
+                            (\x -> x * toRational count)
+                           ) segmentFractions
+        count = length rows
+        segmentFractions = map (readRational . map
+                                (\x -> case x of
+                                            '/' -> '%'
+                                            _ -> x)
+                               ) $ tail args
+        readRational x = read x :: Rational
